ce9ad5a4a7a9b7e070b1eaa4e3117a576fd0097c
shekhar <shekharmourya@bitcot.com>
2023-08-19 07:07:51 +0530

    toggle functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,14 +4,15 @@ import logo from './logo.svg'
 const Card = (props) => {
   return (
     <div>
-        <div class="card" style={{width: "18rem"}}>
-  <img class="card-img-top" src={logo} alt="Card image cap"/>
-  <div class="card-body">
-    <h5 class="card-title">{props.username}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div class="card" style={{ width: "18rem" }}>
+        <img class="card-img-top" src={logo} alt="Card image cap" />
+        <div class="card-body">
+          <h6>{props.number + 1}</h6>
+          <h5 class="card-title">{props.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 const Main = () => {
 
-  const myname  = ["Shekhar", "Farhan","Nakul", "Nirmal","Nilesh"]
-   
+  const myname = ["Shekhar", "Farhan", "Nakul", "Nirmal"]
+
+  const [show, setShow] = useState(false) // Default Value
+  const [color, setColor] = useState("red") //  Default Value
+
+  // if(show === true){
+  //   alert("value is true")
+  // }else{
+  //   alert("value is false")
+  // }
+
+  //  show ? "valuee is true" : "value is false" ---> ternary operators
+
+  console.log("show", show)
   return (
-      <div className='row'>
-        <h2>Main</h2>
-        {myname.map( username =>
-        <div className='col-md-3'>
-        <Card username={username}/>
-        </div>
-        )}
+    <div className='row'>
+      <h2 style={{ color: color }}>Main</h2>
+      <div className='col'>
+        <button onClick={() => setColor("green")} className='btn btn-primary'>change color</button>
+        <button onClick={() => { setShow(!show) }} className='btn btn-primary mt-2 mx-2'>
+          {show ? "Hide cards" : " Show Cards"}</button>
+      </div>
+
+      {show ? myname.map((value, index) => {
+        if (index == 1 || index == 3) {
+          return (<div className='col-md-3'>
+            <Card name={value} number={index} />
+          </div>)
+        } 
+      }
+
+
+      ) : null}
+
+
     </div>
   )
 }
